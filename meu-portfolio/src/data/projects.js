e7a16bd334c60a3c132277b24aec928b41b93d3c
Hugo-Nascimento748 <nascimentohugo748@gmail.com>
2026-03-02 11:16:18 -0300

    Acertando pequenos erros

--- meu-portfolio/src/data/projects.js
+++ meu-portfolio/src/data/projects.js
@@ -1,10 +1,10 @@
 export const projects = [
   {
     id: 1,
-    title: "Projeto Exemplo 1",
-    description: "Breve descrição do que você desenvolveu e o problema que resolveu.",
-    techs: ["React", "Tailwind", "Firebase"],
-    github: "https://github.com/seu-usuario/repo",
+    title: "SchedulerPro",
+    description: "O SchedulerPro é uma plataforma completa de agendamento e gestão para prestadores de serviços. O sistema combina um backend robusto em .NET 9 com um dashboard administrativo moderno e responsivo construído em Tailwind CSS.",
+    techs: ["React", "Tailwind", "C#", ".NET9"],
+    github: "https://github.com/Hugo-Nascimento748/SchedulerPro",
     demo: "https://seu-link-demo.com",
     image: "https://via.placeholder.com/300x200" // Você trocará pelas suas prints
   },
--- meu-portfolio/vite.config.js
+++ meu-portfolio/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Hugo-Nascimento748.github.io/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // Se o seu repositório se chama exatamente "Hugo-Nascimento748.github.io", a base é "/"
+  // Se for qualquer outro nome, use "/nome-do-repo/"
+  base: "/", 
 })
